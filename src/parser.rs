6590d0d676020289cf7a34f35e89cae4fc6a1ef9
David Grankvist <grankvist.david@gmail.com>
2022-06-13 20:30:23 +0200

    parser: add basic string to Song conversion

--- src/parser.rs
+++ src/parser.rs
@@ -3,9 +3,27 @@ use crate::models::*;
 
 pub fn parse_song(raw_song: &str, config: &Config) -> Song {
     println!("Parsing this song:\n{}", &raw_song);
-    Song {
+    let time = TimeSignature::new(config.time.0, config.time.1);
+
+    let bars: Vec<Bar> = raw_song.split("|")
+        .map(| bar | {
+            let chords: Vec<Chord> = bar.split_whitespace()
+                .map(| chord |{
+                    let root = chord.chars().next().expect("Failed to parse chord");
+                    Chord {
+                        duration: 1,
+                        root,
+                    }
+                }).collect();
+            Bar {
+                chords,
+            }
+        }).collect();
+    let song = Song {
         tempo: config.tempo,
-        time: TimeSignature::new(config.time.0, config.time.1),
-        bars: vec![]
-    }
+        time,
+        bars,
+    };
+    dbg!(&song);
+    song
 }
